be2760900237ec0345bc1a0bcb3050c3b8f2a140
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-06-30 15:51:17 +0300

    handlers/pets: fills the handler bodies missing parse and validate calls

--- handlers/pets/delete.go
+++ handlers/pets/delete.go
@@ -1,6 +1,7 @@
 package pets
 
 import (
+	"log/slog"
 	"net/http"
 
 	"github.com/ufukty/gohandlers-petstore/handlers/pets/types"
@@ -15,12 +16,20 @@ func (p *Pets) DeletePet(w http.ResponseWriter, r *http.Request) {
 	bq := &DeletePetRequest{}
 
 	if err := bq.Parse(r); err != nil {
-		//
+		slog.Debug("user error on parsing request", "content", err.Error())
+		http.Error(w, "error on parsing request", 400)
+		return
 	}
 
-	if err := bq.Validate(); err != nil {
-		//
+	if issues := bq.Validate(); issues != nil {
+		if err := serialize(w, issues); err != nil {
+			slog.Error("error on serialization validation error", "content", err)
+			http.Error(w, "error on serialization validation error", 500)
+		} else {
+			slog.Debug("user error on validation", "length", len(issues))
+		}
+		return
 	}
 
-	// implementation here
+	// ...
 }
--- handlers/pets/get.go
+++ handlers/pets/get.go
@@ -1,6 +1,7 @@
 package pets
 
 import (
+	"log/slog"
 	"net/http"
 
 	"github.com/ufukty/gohandlers-petstore/handlers/pets/dto"
@@ -17,21 +18,32 @@ type GetPetResponse struct {
 
 // GET /pets/{id}
 func (p *Pets) GetPet(w http.ResponseWriter, r *http.Request) {
-
 	bq := &GetPetRequest{}
 
 	if err := bq.Parse(r); err != nil {
-		//
+		slog.Debug("user error on parsing request", "content", err.Error())
+		http.Error(w, "error on parsing request", 400)
+		return
 	}
 
-	if err := bq.Validate(); err != nil {
-		//
+	if issues := bq.Validate(); issues != nil {
+		if err := serialize(w, issues); err != nil {
+			slog.Error("error on serialization validation error", "content", err)
+			http.Error(w, "error on serialization validation error", 500)
+		} else {
+			slog.Debug("user error on validation", "length", len(issues))
+		}
+		return
 	}
 
-	// implementation here
+	// ...
 
-	bs := &GetPetResponse{}
+	bs := &GetPetResponse{
+		Pet: dto.Pet{},
+	}
 	if err := bs.Write(w); err != nil {
-		//
+		slog.Debug("user error on serializing response", "content", err.Error())
+		http.Error(w, "serializing response", 400)
+		return
 	}
 }
